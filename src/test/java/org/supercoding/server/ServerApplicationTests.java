9299efa4c7f8287a701ce5384cc1d61a7a5e1587
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-01-02 16:47:41 +0900

    Setting

---
+++ src/test/java/org/supercoding/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.supercoding.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
