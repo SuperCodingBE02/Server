35ad3782e31763757aafb6f17e221914b6606fca
unknown <kse5020@naver.com>
2024-01-05 21:43:22 +0900

    feat: swagger 추가

--- src/main/java/org/supercoding/server/web/dto/PostRequestDto.java
+++ src/main/java/org/supercoding/server/web/dto/PostRequestDto.java
@@ -1,5 +1,6 @@
 package org.supercoding.server.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,10 +14,14 @@ import java.util.Date;
 @NoArgsConstructor
 @ToString
 public class PostRequestDto {
-
+    //TODO string으로 바꿔야할까?
+    @Schema(description = "작성자", example = "홍길동")
     private UserEntity user;
+    @Schema(description = "글 제목", example = "곱창마렵다")
     private String title;
+    @Schema(description = "글 본문")
     private String content;
+    @Schema(description = "작성일")
     private Date createAt;
 
 
